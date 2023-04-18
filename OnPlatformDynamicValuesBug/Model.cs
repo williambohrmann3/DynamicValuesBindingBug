94d992be98525fc16e657eb6050044e57ffa4cf9
William Bohrmann <wbohrmann@esri.com>
2023-04-18 12:54:39 -0700

    no more AppThemeBinding; Picker added

--- OnPlatformDynamicValuesBug/Model.cs
+++ OnPlatformDynamicValuesBug/Model.cs
@@ -5,9 +5,14 @@
         private Color _color;
         public Color Text_Color { get => _color; set => _color = value; }
 
+        private IList<string> _items;
+        public IList<String> Items_Source { get => _items; set => _items = value; }
+
         public Model() 
         {
             Text_Color = Colors.Orange;
+            Items_Source = new List<String>() { "item1", "item2", "item3" };
+            
         }
     }
 }
