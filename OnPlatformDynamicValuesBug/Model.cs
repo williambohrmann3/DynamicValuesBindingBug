c550d79ceb364d9e795535028829bb04f763ca71
William Bohrmann <wbohrmann@esri.com>
2023-04-18 13:19:49 -0700

    CheckBox and Button controls

--- OnPlatformDynamicValuesBug/Model.cs
+++ OnPlatformDynamicValuesBug/Model.cs
@@ -8,11 +8,18 @@
         private IList<string> _items;
         public IList<String> Items_Source { get => _items; set => _items = value; }
 
+        private bool _checked;
+        public bool Is_Checked { get => _checked; set => _checked = value; }
+
+        private double _width;
+        public double Border_Width { get => _width; set => _width = value; }
+
         public Model() 
         {
             Text_Color = Colors.Orange;
             Items_Source = new List<String>() { "item1", "item2", "item3" };
-            
+            Is_Checked = true;
+            Border_Width = 40.0;
         }
     }
 }
